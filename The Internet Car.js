c1b95ddd9ca6a0d715a9ec72bb16a71bcbf91952
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-03-06 11:01:48 +0000

    Update The Internet Car.js

--- The Internet Car.js
+++ The Internet Car.js
@@ -1,3 +1,4 @@
+function checkDriverAge(){
 alert("Welcome to the keyless car - let's go!");
 var age = prompt("Wait, how old are you?");
 
@@ -7,6 +8,7 @@ if (age > 18){
 	alert("Sorry, you're too young to drive. Powering off");
 } else if (age === 18) {
 		alert("Well done on passing your test, time to drive!");
-} else {
-	alert("Hmmm, something is not right here!");
+} 
 }
+
+checkDriverAge();
